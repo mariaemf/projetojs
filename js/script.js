206fda61ce1ec922d08adca80332a545d0a400ce
mariaemf <mariaeduardamartinsf@gmail.com>
2022-08-03 22:07:41 -0300

    novas estilizacoes ao dropdown da pagina

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initDropDownMenu from "./modules/dropdown-menu.js";
 
 initAccordion();
 initAnimacaoScroll();
@@ -11,3 +12,4 @@ initScrollSuave();
 initTabNav();
 initModal();
 initTooltip();
+initDropDownMenu();
